bbe14be74d826be9ee3abf94dd33136ebde743d5
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-10-30 16:25:01 +0300

    add tests cases

--- src/test/java/com/github/lombrozo/testnames/PatternTest.java
+++ src/test/java/com/github/lombrozo/testnames/PatternTest.java
@@ -11,7 +11,32 @@ public class PatternTest {
     @CsvSource({
         "test, false",
         "canTest, false",
-        "invokesLastSuccessfully, true"
+        "execSuccessfully, false",
+        "fullCompilingLifecycleSuccessfully, false",
+        "_execsCorrectly, false",
+        "$execsCorrectly, false",
+        "sTestMy false",
+        "$sAnotherCase false",
+        "_sCrack false",
+        "ssssText false",
+        "testContentOfCachedFile, false",
+        "testContentOfNoCacheFile, false",
+        "testPacks, false",
+        "bigSlowTest, false",
+        "existsTest, false",
+        "existsInDirTest, false",
+        "InvokesLastSuccessfully false",
+        "invokesLastSuccessfully, true",
+        "existsInDirDifferentEncryptionTest, true",
+        "readsHashByNonExistedTag, true",
+        "readsCorrectHashByTagFromSimpleString, true",
+        "readsCorrectHashByTagFromFile, true",
+        "copiesSources true",
+        "findsDirs, true",
+        "executesDiscoveryPhaseForCorrectEoPrograms, true",
+        "readsFromExistingFile, true",
+        "saves, true",
+        "returnsRelativePathOfCurrentWorkingDirectory, true"
     })
     public void validatesCorrectly(String name, boolean expected) {
         Assertions.assertEquals(expected, new Pattern(name).valid());
