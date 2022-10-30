bd41cbb1aef6d78124580c8161114aaa6a5dd855
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-10-30 17:04:56 +0300

    add naive pattern implementation

--- src/main/java/com/github/lombrozo/testnames/Pattern.java
+++ src/main/java/com/github/lombrozo/testnames/Pattern.java
@@ -8,7 +8,62 @@ public class Pattern {
         this.name = name;
     }
 
-    public boolean valid(){
+    public boolean valid() {
+        return notContainsTest()
+            && notCamelCase()
+            && notUsesSpecialCharacters()
+            && notSpam()
+            && presentTense();
+    }
+
+    private boolean notSpam() {
+        int stack = 0;
+        char prev = '!';
+        for (final char c : name.toCharArray()) {
+            if(c == prev){
+                stack++;
+            } else {
+                stack = 0;
+                prev = c;
+            }
+            if(stack > 2){
+                return false;
+            }
+        }
         return true;
+    }
+
+    private boolean notContainsTest() {
+        return !name.matches(".*[Tt][Ee][Ss][Tt].*");
+    }
+
+    private boolean notUsesSpecialCharacters() {
+        return !name.contains("$") && !name.contains("_");
+    }
+
+    private boolean notCamelCase() {
+        int stack = 0;
+        for (final char c : name.toCharArray()) {
+            if (Character.isUpperCase(c) && stack == 0) {
+                return false;
+            } else if (stack != 0) {
+                stack = 0;
+            }
+            stack++;
+        }
+        return true;
+    }
+
+    private boolean presentTense() {
+        final char[] chars = name.toCharArray();
+        char prev = '!';
+        for (final char c : chars) {
+            if (Character.isUpperCase(c)) {
+                return prev == 's';
+            } else {
+                prev = c;
+            }
+        }
+        return prev == 's';
     }
 }
