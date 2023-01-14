b3a6f78db7e771d1b253492fe6b04432092a2b9d
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-01-14 13:24:51 +0300

    feat(#9): cover different cases for JavaTestCode

--- src/test/java/com/github/lombrozo/testnames/JavaTestCodeTest.java
+++ src/test/java/com/github/lombrozo/testnames/JavaTestCodeTest.java
@@ -6,19 +6,51 @@ import org.cactoos.bytes.BytesOf;
 import org.cactoos.io.ResourceOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 class JavaTestCodeTest {
 
     @Test
-    void getsNamesSuccessfully(@TempDir final Path temp) throws Exception {
-        final Path path = temp.resolve("TestExample.java");
-        Files.write(path, new BytesOf(new ResourceOf("TestExample.java")).asBytes());
+    void getsNamesFromSimpleTest(@TempDir final Path temp) throws Exception {
+        final String java = "TestSimple.java";
+        final Path path = temp.resolve(java);
+        Files.write(path, new BytesOf(new ResourceOf(java)).asBytes());
         final Names names = new JavaTestCode(path);
         MatcherAssert.assertThat(
             names.names(),
             Matchers.containsInAnyOrder("creates", "removes", "updates")
+        );
+    }
+
+    @Test
+    void getsNamesFromParameterizedTest(@TempDir final Path temp) throws Exception {
+        final String java = "TestParameterized.java";
+        final Path path = temp.resolve(java);
+        Files.write(path, new BytesOf(new ResourceOf(java)).asBytes());
+        final Names names = new JavaTestCode(path);
+        MatcherAssert.assertThat(
+            names.names(),
+            Matchers.containsInAnyOrder("checksCases")
+        );
+    }
+
+    @Test
+    void throwsExceptionIfFileNotFound(@TempDir final Path temp) throws Exception {
+        Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> new JavaTestCode(temp.resolve("TestNotFound.java")).names()
+        );
+    }
+
+    @Test
+    void throwsExceptionIfFileIsNotJava(@TempDir final Path temp) throws Exception {
+        final Path path = temp.resolve("TestNotJava.txt");
+        Files.write(path, "Not Java".getBytes());
+        Assertions.assertThrows(
+            IllegalStateException.class,
+            () -> new JavaTestCode(path).names()
         );
     }
 
---
+++ src/test/resources/TestParameterized.java
@@ -0,0 +1,10 @@
+package com.github.lombrozo.testnames;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class TestParameterized {
+    @ParameterizedTest
+    void checksCases(final String param) {
+    }
+}
