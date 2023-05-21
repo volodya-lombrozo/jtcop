c5dff15c1affd98dabd716ccbebf258c424b55ca
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-05-21 15:21:48 +0300

    feat(#136): Remove the puzzle

--- src/main/java/com/github/lombrozo/testnames/ValidateMojo.java
+++ src/main/java/com/github/lombrozo/testnames/ValidateMojo.java
@@ -62,9 +62,6 @@ public final class ValidateMojo extends AbstractMojo {
 
     /**
      * The rules that have to be excluded from execution.
-     * @todo #129:90min Add integration tests for ValidateMojo exclusions.
-     *  We have to add integration tests for ValidateMojo exclusions. The integration test
-     *  will check that we don't apply rules that are excluded.
      */
     @Parameter(property = "exclusions")
     private String[] exclusions;
