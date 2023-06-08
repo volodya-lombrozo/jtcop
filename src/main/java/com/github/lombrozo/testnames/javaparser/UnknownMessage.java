2dd98a4301fa5f43e3dfedaf17aeab9b2bec616c
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-08 18:13:22 +0300

    feat(#165): fix all qulice suggestions

--- src/main/java/com/github/lombrozo/testnames/javaparser/UnknownMessage.java
+++ src/main/java/com/github/lombrozo/testnames/javaparser/UnknownMessage.java
@@ -1,19 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 Volodya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.lombrozo.testnames.javaparser;
 
 import java.util.Optional;
 
+/**
+ * Unknown message.
+ *
+ * @since 0.1.15
+ */
 final class UnknownMessage {
 
+    /**
+     * The message.
+     */
     private final String msg;
 
+    /**
+     * Constructor.
+     */
     UnknownMessage() {
         this("Unknown message. The message will be known only in runtime");
     }
 
+    /**
+     * Constructor.
+     * @param message The message.
+     */
     private UnknownMessage(final String message) {
         this.msg = message;
     }
 
+    /**
+     * Retrieves message as optional.
+     * @return Optional message.
+     */
     Optional<String> message() {
         return Optional.of(this.msg);
     }
