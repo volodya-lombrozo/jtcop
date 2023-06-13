fec28c405cf2d2be99bb54afc255bc8c3cc92bf4
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-13 18:10:57 +0300

    feat(#170): enable test and remove the puzzle

--- src/test/java/com/github/lombrozo/testnames/javaparser/AssertionOfJavaParserTest.java
+++ src/test/java/com/github/lombrozo/testnames/javaparser/AssertionOfJavaParserTest.java
@@ -30,17 +30,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * Test for {@link AssertionOfJavaParser}.
  *
  * @since 0.1.15
- * @todo #164:30min Enable AssertionOfJavaParserTest#extractMessagesFromAllAssertions test.
- *  This test is disabled because it fails. Apparently, we have the bug in the parser of JUnit
- *  assertions. We need to fix it. After that, we should enable this test:
- *  {@link AssertionOfJavaParserTest#extractsMessagesFromAllAssertions()}
  */
 class AssertionOfJavaParserTest {
 
@@ -94,7 +89,6 @@ class AssertionOfJavaParserTest {
     }
 
     @Test
-    @Disabled
     void extractsMessagesFromAllAssertions() {
         final List<AssertionOfJavaParser> all = AssertionOfJavaParserTest
             .method("severalFrameworks")
