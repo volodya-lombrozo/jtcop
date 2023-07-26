5b79536292f6a8d2332f949fbd3100c36b033ade
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-07-26 15:56:50 +0300

    feat(#232): Remove the puzzle for #232

--- src/test/java/com/github/lombrozo/testnames/javaparser/CharacteristicsJavaParserTest.java
+++ src/test/java/com/github/lombrozo/testnames/javaparser/CharacteristicsJavaParserTest.java
@@ -31,10 +31,6 @@ import org.junit.jupiter.api.Test;
  * Test case for {@link CharacteristicsJavaParser}.
  *
  * @since 0.1.19
- * @todo #194:30min Add tests for all the methods from CharacteristicsJavaParser class.
- *  The tests should be added to CharacteristicsJavaParserTest class for all
- *  methods from {@link com.github.lombrozo.testnames.javaparser.CharacteristicsJavaParser}.
- *  When we implement the tests, we should remove the that puzzle from this class.
  */
 final class CharacteristicsJavaParserTest {
 
