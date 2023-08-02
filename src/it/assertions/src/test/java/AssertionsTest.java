a61d7cadcdeeb699cbe93f2bc6f31f75f29f4c0a
l3r8yJ <l3r8y@duck.com>
2023-08-02 13:14:21 +0300

    Change suppression

--- src/it/assertions/src/test/java/AssertionsTest.java
+++ src/it/assertions/src/test/java/AssertionsTest.java
@@ -35,7 +35,7 @@ class AssertionsTest {
     private final static String CONSTANT = "Message";
 
     @Test
-    @SuppressWarnings("JTCOP.RuleAssertTrueHitter")
+    @SuppressWarnings("JTCOP.RuleLineHitter")
     void checksJUnitAssertions() {
         Assertions.assertEquals("1", "1", message());
         Assertions.assertEquals("1", "1", "Message");
