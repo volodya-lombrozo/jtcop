83c795fd4e4daa9ad70ed19e896f77da08506368
h1alexbel <aliaksei.bialiauski@hey.com>
2024-07-11 14:49:55 +0300

    feat(#393): Rule

---
+++ src/main/java/com/github/lombrozo/testnames/rules/RuleTestCaseContainsMockery.java
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2024 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.lombrozo.testnames.rules;
+
+import com.github.lombrozo.testnames.Complaint;
+import com.github.lombrozo.testnames.Rule;
+import com.github.lombrozo.testnames.TestCase;
+import com.github.lombrozo.testnames.complaints.ComplaintLinked;
+import com.github.lombrozo.testnames.javaparser.NumberOfMockitoMocks;
+import java.util.Collection;
+
+/**
+ * Mockery rule.
+ *
+ * @since 1.3.4
+ */
+public final class RuleTestCaseContainsMockery implements Rule {
+
+    /**
+     * Test case.
+     */
+    private final TestCase test;
+
+    /**
+     * Allowed number of mocks.
+     */
+    private final Long allowed;
+
+    /**
+     * Ctor.
+     *
+     * @param tst   Test case
+     * @param allwd Allowed number of mocks.
+     */
+    public RuleTestCaseContainsMockery(final TestCase tst, final Long allwd) {
+        this.test = tst;
+        this.allowed = allwd;
+    }
+
+    @Override
+    public Collection<Complaint> complaints() {
+        final Long mocks = new NumberOfMockitoMocks(this.test).value();
+        return new RuleConditional(
+            () -> mocks > this.allowed,
+            new ComplaintLinked(
+                String.format(
+                    "Test case '%s' contains excessive number of mocks: %s. max allowed: %s",
+                    this.test.name(),
+                    mocks,
+                    this.allowed
+                ),
+                "Simplify mocking in test case",
+                this.getClass(),
+                "mockery.md"
+            )
+        ).complaints();
+    }
+}
---
+++ src/test/java/com/github/lombrozo/testnames/rules/RuleTestCaseContainsMockeryTest.java
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2024 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.lombrozo.testnames.rules;
+
+import com.github.lombrozo.testnames.Complaint;
+import com.github.lombrozo.testnames.TestCase;
+import com.github.lombrozo.testnames.javaparser.JavaTestClasses;
+import org.cactoos.list.ListOf;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link RuleTestCaseContainsMockeryTest}.
+ *
+ * @since 1.3.4
+ */
+final class RuleTestCaseContainsMockeryTest {
+
+    @Test
+    void complaintsAboutMockery() {
+        final TestCase test = new ListOf<>(
+            JavaTestClasses.MOCKERY_TEST.toTestClass().all()
+        ).get(0);
+        final Complaint complaint = new ListOf<>(
+            new RuleTestCaseContainsMockery(test, 2L).complaints()
+        ).get(0);
+        final String text =
+            "'testsSomething' contains excessive number of mocks: 3. max allowed: 2";
+        MatcherAssert.assertThat(
+            String.format(
+                "Complaint received: %s, but message does not contain text '%s'",
+                complaint,
+                text
+            ),
+            complaint.message(),
+            Matchers.containsString(text)
+        );
+    }
+}
